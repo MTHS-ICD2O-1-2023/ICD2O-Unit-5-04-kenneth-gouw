10e4de2d4f8a0353069854aad2f35856b43c94f3
kenneth-gouw <159185824+kenneth-gouw@users.noreply.github.com>
2024-05-01 18:13:54 +0000

    e

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Kenneth All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Kenneth
+// Created on: April 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function findDiscountClicked() {
+  const userAge = parseInt(document.getElementById("age-entered").value)
+
+  const currentDate = (document.getElementById("day-entered").value)
+
+  if (((userAge > 12) && (userAge < 21)) || ((currentDate == "Tuesday") || (currentDate == "Thursday"))) {
+    document.getElementById("answer").innerHTML = "You are eligible for the discount"
+  } else {
+    document.getElementById("answer").innerHTML = "You are Not eligible for the discount"
+  }
+
+  // Output the answer to the user in the class "answer"
+}
